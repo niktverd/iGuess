06e638a9fb6091a8b18f13688435587ed2d990b9
Nik Tverd <nn.tverd@gmail.com>
2023-10-26 13:37:48 +0600

    fixup! feat: paypal payment added

--- pages/payments/index.tsx
+++ pages/payments/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+import {PayPalButtons, PayPalScriptProvider} from '@paypal/react-paypal-js';
 import type {NextPage} from 'next';
 
 import {Page} from '../../src/containers/Page/Page';
@@ -12,15 +12,30 @@ const paypalClienId = process.env.PAYPAL_CLIENT_ID;
 const Tasks: NextPage = () => {
     return (
         <Page>
-            {paypalClienId ? <PayPalScriptProvider options={{ clientId: paypalClienId, intent: 'subscription', vault: true, currency: 'USD' }}>
-            <PayPalButtons style={{ layout: "horizontal", shape: "pill", label: "subscribe", color: "black" }} createSubscription={(_data, actions) => {
-			return actions.subscription
-				.create({
-					plan_id: "P-8YP55027XM3365445MU4VRZQ",
-				});
-		}} />
-        </PayPalScriptProvider> : null}
-           
+            {paypalClienId ? (
+                <PayPalScriptProvider
+                    options={{
+                        clientId: paypalClienId,
+                        intent: 'subscription',
+                        vault: true,
+                        currency: 'USD',
+                    }}
+                >
+                    <PayPalButtons
+                        style={{
+                            layout: 'horizontal',
+                            shape: 'pill',
+                            label: 'subscribe',
+                            color: 'black',
+                        }}
+                        createSubscription={(_data, actions) => {
+                            return actions.subscription.create({
+                                plan_id: 'P-8YP55027XM3365445MU4VRZQ',
+                            });
+                        }}
+                    />
+                </PayPalScriptProvider>
+            ) : null}
         </Page>
     );
 };
